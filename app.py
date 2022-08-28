f8522dd62fb05db031dffe6d8292e5184aa0cc76
obov <obov3e@gmail.com>
2022-08-28 22:23:40 +0900

    Im Home

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask ,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "hi"
+    return render_template("home.html")
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
