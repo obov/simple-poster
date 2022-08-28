f6cb8fc6be48b1e80cff9067d50e15448902f7fe
obov <obov3e@gmail.com>
2022-08-29 00:52:10 +0900

    list done

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,21 @@
+const postList = $("#postList");
+
 $(document).ready(function () {
-  console.log("hi");
+  $.ajax({
+    type: "GET",
+    url: "https://quote-garden.herokuapp.com/api/v3/quotes",
+    data: {},
+    success: function ({ data }) {
+      postList.empty();
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        const { quoteText, quoteAuthor } = data[i];
+        const liHuggingData = $(`<li>${quoteText}</li>`);
+        postList.append(liHuggingData);
+      }
+    },
+    error: function (error) {
+      console.log("error : ", error);
+    },
+  });
 });
