ae4cd4ae1cacf24fcac14663d793c8db172039e6
ninthsun91 <ninthsun91@gmail.com>
2022-08-29 13:53:42 +0900

    assign tag ids & minor changes in names

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ def home():
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
-@app.route("/post/submit", methods=["POST"])
+@app.route("/poster", methods=["POST"])
 def post_submit():
     return jsonify({"msg":"success"})
