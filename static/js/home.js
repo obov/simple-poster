a7ee97edde8c30eb18f37b82e324f746dafcad4f
obov <obov3e@gmail.com>
2022-08-29 21:39:02 +0900

    arrange home js

--- static/js/home.js
+++ static/js/home.js
@@ -11,19 +11,22 @@ $(document).ready(function () {
       console.log(data);
       for (let i = 0; i < data.length; i++) {
         const { quoteText, quoteAuthor, _id } = data[i];
+
         const liWrapper = $(`<li id="poster${_id}"></li>`);
         const ancherPoster = $(`<a href="/poster?id=${_id}">${quoteText}</a>`);
         const iconWrapper = $("<div class='icon-wrapper'></div>");
         const editIcon = $("<i class='bi bi-pencil-square'></i>");
         const deleteIcon = $("<i class='bi bi-trash3'></i>");
+
         liWrapper.append(ancherPoster);
         iconWrapper.append(editIcon);
         iconWrapper.append(deleteIcon);
         liWrapper.append(iconWrapper);
+        postList.append(liWrapper);
+
         ancherPoster.on("click", () => console.log("you clicked", _id));
         editIcon.on("click", () => console.log("you clicked", "edit"));
         deleteIcon.on("click", () => console.log("you clicked", "delete"));
-        postList.append(liWrapper);
       }
     },
     error: function (error) {
