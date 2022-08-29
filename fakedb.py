4fc7884fbb83ee1b51a029c45c591418ac94a78f
obov <obov3e@gmail.com>
2022-08-30 08:34:55 +0900

    isolate fake db from app.py

---
+++ fakedb.py
@@ -0,0 +1,33 @@
+'''임시 db입니다'''
+fake_db = {
+    "poster": [
+        {
+            "id":1,"title":"All diseases run into one, old age.","content":"All diseases run into one, old age."
+        },
+        {
+            "id":2,"title":"Alas, after a certain age every man is","content":"Alas, after a certain age every man is responsible for his face."
+        },
+        {
+            "id":3,"title":"When grace is joined with wrinkles, it is adorable.","content":"When grace is joined with wrinkles, it is adorable. There is an unspeakable dawn in happy old age."
+        },
+        {
+            "id":4,"title":"Age is not a particularly interesting subject.","content":"Age is not a particularly interesting subject. Any… get old. All you have to do is live long enough"
+        },
+        {
+            "id":5,"title":"My notion of a wife at 40 is that a man should be able to change her","content":"My notion of a wife at 40 is that a man should be able to change her, like a bank note, for two 20s."
+        },
+        {
+            "id":6,"title":"Aging seems to be the only available way ","content":"Aging seems to be the only available way to live a long life."
+        },
+        {
+            "id":7,"title":"You know you're getting old when all the names ","content":"You know you're getting old when all the names in your black book have M. D. after them."
+        },
+        {
+            "id":8,"title":"Middle age is youth without levity,","content":"Middle age is youth without levity, and age without decay."
+        },
+        {
+            "id":9,"title":"The answer to old age is to keep one's mind busy","content":"The answer to old age is to keep one's mind busy a…ng a new house when he was dying of tuberculosis."
+        },
+        
+    ]
+}
