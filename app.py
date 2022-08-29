3827ab0ea4f78566e3dc379f5bceb7b00adce1eb
obov <obov3e@gmail.com>
2022-08-29 16:40:21 +0900

    __name__ __main__ should be bottom line

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
 
 @app.route("/poster", methods=["POST"])
 def post_submit():
     return jsonify({"msg":"success"})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
