337fba11c151f400823bb4adf7c83216e4a404fd
obov <obov3e@gmail.com>
2022-08-29 16:32:57 +0900

    change css / add icons

--- static/js/home.js
+++ static/js/home.js
@@ -11,9 +11,17 @@ $(document).ready(function () {
       console.log(data);
       for (let i = 0; i < data.length; i++) {
         const { quoteText, quoteAuthor, _id } = data[i];
-        const liHuggingData = $(`<li id="poster${_id}"><a href="/poster?id=${_id}">${quoteText}</a></li>`);
-        liHuggingData.on("click", () => console.log("you clicked", _id));
-        postList.append(liHuggingData);
+        const liWrapper = $(`<li id="poster${_id}"></li>`);
+        const ancherPoster = $(`<a href="/poster?id=${_id}">${quoteText}</a>`);
+        const iconWrapper = $("<div class='icon-wrapper'></div>");
+        const editIcon = $("<i class='bi bi-pencil-square'></i>");
+        const deleteIcon = $("<i class='bi bi-trash3'></i>");
+        liWrapper.append(ancherPoster);
+        iconWrapper.append(editIcon);
+        iconWrapper.append(deleteIcon);
+        liWrapper.append(iconWrapper);
+        ancherPoster.on("click", () => console.log("you clicked", _id));
+        setTimepostList.append(liWrapper);
       }
     },
     error: function (error) {
