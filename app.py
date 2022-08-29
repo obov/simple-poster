97f54791dbf35e5e7e02f981d08255f0aadd9db5
ninthsun91 <ninthsun91@gmail.com>
2022-08-29 13:47:03 +0900

    create post_submit layout

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+@app.route("/post/submit", methods=["POST"])
+def post_submit():
+    return jsonify({"msg":"success"})
