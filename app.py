29c6ed3354d5391181b3d4cb3843357d239c57e6
obov <obov3e@gmail.com>
2022-08-29 20:28:49 +0900

    bootstrap link location, some css change conflicted with bootstrap, render template

--- app.py
+++ app.py
@@ -4,11 +4,21 @@ from pymongo import MongoClient
 client = MongoClient('URL')
 db = client.simple_poster
 
-
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return render_template("new_poster.html")
+    return render_template("home.html")
+
+@app.route("/poster", methods=["GET"])
+def post_get():
+
+    ## validator
+    id = request.args["id"]
+    post = db.poster.find_one({"id":id})
+    
+
+    return jsonify({"msg":"success"}) # @app.route('/') 에 return render_template("new_poster.html")를 하셔서 수정했습니다 - 임요한
 
 
 @app.route("/poster/submit", methods=["POST"])
@@ -34,15 +44,6 @@ def post_submit():
     return jsonify({"msg":"success"})
 
 
-@app.route("/poster", methods=["GET"])
-def post_get():
-
-    ## validator
-    id = request.args["id"]    
-    post = db.poster.find_one({"id":id})
-    
-
-    return jsonify({"msg":"success"})
 
 
 if __name__ == '__main__':
