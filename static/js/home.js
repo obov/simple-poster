51646e3356d0f43dcc120c71d5b7ad066e2542aa
obov <obov3e@gmail.com>
2022-08-30 20:10:54 +0900

    refactor

--- static/js/home.js
+++ static/js/home.js
@@ -1,15 +1,19 @@
 const postList = $("#posterList");
 const logo = $("#logo");
 
+const tag = (tagName) => $(`<${tagName}></${tagName}>`);
+
 const listingData = (tagToAppend, data) => {
   for (let i = 0; i < data.length; i++) {
     const { content, title, id } = data[i];
     const qs = encodeURIComponent(`id=${id}`);
-    const liWrapper = $(`<li id="poster${id}"></li>`);
-    const ancherPoster = $(`<a href="/poster?${qs}">${title}</a>`);
-    const iconWrapper = $("<div class='icon-wrapper'></div>");
-    const editIcon = $("<i class='bi bi-pencil-square'></i>");
-    const deleteIcon = $("<i class='bi bi-trash3'></i>");
+    const liWrapper = tag("li").attr("id", "poster" + id);
+    const ancherPoster = tag("a")
+      .attr("href", "/poster?" + qs)
+      .text(title);
+    const iconWrapper = tag("div").addClass("icon-wrapper");
+    const editIcon = tag("i").addClass("bi bi-pencil-square");
+    const deleteIcon = tag("i").addClass("bi bi-trash3");
 
     liWrapper.append(ancherPoster);
     iconWrapper.append(editIcon);
@@ -17,7 +21,6 @@ const listingData = (tagToAppend, data) => {
     liWrapper.append(iconWrapper);
     tagToAppend.append(liWrapper);
 
-    ancherPoster.on("click", () => console.log("you clicked", id));
     editIcon.on("click", () => console.log("you clicked", "edit"));
     deleteIcon.on("click", () => console.log("you clicked", "delete"));
   }
