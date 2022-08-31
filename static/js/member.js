fac91c5b1f586e87686a8d3c67e2539de14fb7a5
ninthsun91 <ninthsun91@gmail.com>
2022-08-31 11:21:30 +0900

    signup&signin format check

--- static/js/member.js
+++ static/js/member.js
@@ -1,50 +1,51 @@
+$('#submit').click(()=>{
+  const username = $('input[name="user_ID"]').val();
+  const password = $('input[name="user_PW1"]').val();
+  const password2 = $('input[name="user_PW2"]').val();
 
-  $('#submit').click(function () {
-    const username = $('input[name="user_ID"]').val();
-    const password = $('input[name="user_PW1"]').val();
-    const password2 = $('input[name="user_PW2"]').val();
+  if (checkFormat(username,"아이디")) return;
+  if (checkFormat(password,"비밀번호")) return;
+  if (password !== password2) {
+    alert('비밀번호 정확히 입력해주세요');
+    $(`#password2-txt`).addClass("help-format")
+    $(`#password2`).focus();
+    return;
+  }
 
-    if (username === '') {
-      alert('아이디 입력해주세요');
-      return false;
-    }
-
-    if (password === '') {
-      alert('비밀번호 입력해주세요');
-      return false;
-    }
-
-    if (password !== password2) {
-      alert('비밀번호 정확히 입력해주세요');
-      return false;
-    }
-
-    $.post('http://localhost:5000/user/signup', {
-      username: username,
-      password: password,
-    }, (response)=>{
-      console.log(response.msg)
-    });
+  $.post('http://localhost:5000/user/signup', {
+    username: username,
+    password: password,
+  }, (response)=>{
+    window.location.replace("/");
   });
+});
 
-  $('#login').click(function (e) {
-    console.log("Click")
-    const username = $('#username').val();
-    const password = $('#password').val();
-    if (username == '') {
-      alert('아이디 입력해주세요');
-      return false;
-    }
-    if (password == '') {
-      alert('비밀번호 입력해주세요');
-      return false;
-    }
+$('#login').click(()=>{
+  console.log("Click")
+  const username = $('#username').val();
+  const password = $('#password').val();
+  if (checkFormat(username,"아이디")) return;
+  if (checkFormat(password,"비밀번호")) return;
 
-    $.post('http://localhost:5000/user/login', {
-      username: username,
-      password: password,
-    }, (response)=>{
-      // 서버에서 암호화한 로그인 토큰을 다시 건네받고 브라우저 쿠키에 저장해야해요.
-    });
-  })
+  $.post('http://localhost:5000/user/login', {
+    username: username,
+    password: password,
+  }, (response)=>{
+    window.location.replace("/");
+  });
+})
 
+function checkFormat(val,txt) {
+  const regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9]{6,15}$/;
+  if (val === '') {
+    alert(`${txt}를 입력해주세요`);
+    $(`#${txt}-txt`).addClass("help-format")
+    $(`#${txt}`).focus();
+    return true;
+  } else if (!regExp.test(val)) {
+    alert(`${txt}는 6~15자로 영문,숫자만 사용 가능합니다.`)
+    $(`#${txt}-txt`).addClass("help-format")
+    $(`#${txt}`).focus();
+    return true;
+  } else return false;
+}
